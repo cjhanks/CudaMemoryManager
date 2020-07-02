5a904da91d78ef20cee5b7cb6e96c8ddbd55c826
Zeek Oni <web@cjhanks.name>
2020-07-02 08:56:32 -0700

    Create all broadcast operator options.

--- test/cmm/matrix.cu
+++ test/cmm/matrix.cu
@@ -12,7 +12,6 @@ TEST(MatrixCU, ExplicitCallingConvention) {
       cmm::BroadcastEqu<unsigned>(),
       1);
   pv.Memory().TransferToCPU();
-
   cmm::Stream::This().Synchronize();
   for (std::size_t n = 0; n < pv.Size(); ++n) {
     ASSERT_EQ(pv.At(n), 1);
@@ -64,5 +63,116 @@ TEST(MatrixCU, ExplicitCallingConvention) {
   cmm::Stream::This().Synchronize();
   for (std::size_t n = 0; n < pv.Size(); ++n) {
     ASSERT_EQ(pv.At(n), 12);
+  }
+}
+
+TEST(MatrixCU, BroadcastInPlaceNormalConvention) {
+  cmm::PinVector<unsigned> pv(100);
+
+  // Assignment test
+  pv = 1;
+  pv.Memory().TransferToCPU();
+  cmm::Stream::This().Synchronize();
+  for (std::size_t n = 0; n < pv.Size(); ++n) {
+    ASSERT_EQ(pv.At(n), 1);
+  }
+
+  // Add test
+  pv.Memory().TransferToGPU();
+  pv += 10;
+  pv.Memory().TransferToCPU();
+
+  cmm::Stream::This().Synchronize();
+  for (std::size_t n = 0; n < pv.Size(); ++n) {
+    ASSERT_EQ(pv.At(n), 11);
+  }
+
+  // Sub test
+  pv.Memory().TransferToGPU();
+  pv -= 5;
+  pv.Memory().TransferToCPU();
+
+  cmm::Stream::This().Synchronize();
+  for (std::size_t n = 0; n < pv.Size(); ++n) {
+    ASSERT_EQ(pv.At(n), 6);
+  }
+
+  // Mul test
+  pv.Memory().TransferToGPU();
+  pv *= 4;
+  pv.Memory().TransferToCPU();
+
+  cmm::Stream::This().Synchronize();
+  for (std::size_t n = 0; n < pv.Size(); ++n) {
+    ASSERT_EQ(pv.At(n), 24);
+  }
+
+  // Div test
+  pv.Memory().TransferToGPU();
+  pv /= 2;
+  pv.Memory().TransferToCPU();
+
+  cmm::Stream::This().Synchronize();
+  for (std::size_t n = 0; n < pv.Size(); ++n) {
+    ASSERT_EQ(pv.At(n), 12);
+  }
+}
+
+TEST(MatrixCU, BroadcastNormalConvention) {
+  cmm::PinVector<unsigned> pv(100);
+  cmm::PinVector<unsigned> pr;
+
+  // Assignment test
+  pv = 1;
+  pv.Memory().TransferToCPU();
+  cmm::Stream::This().Synchronize();
+  for (std::size_t n = 0; n < pv.Size(); ++n) {
+    ASSERT_EQ(pv.At(n), 1);
+  }
+
+  // Add test
+  pv.Memory().TransferToGPU();
+  pr = pv + 10;
+  pr.Memory().TransferToCPU();
+
+  cmm::Stream::This().Synchronize();
+  for (std::size_t n = 0; n < pv.Size(); ++n) {
+    ASSERT_EQ(pr.At(n), 11);
+  }
+
+  pv = std::move(pr);
+
+  // Sub test
+  pv.Memory().TransferToGPU();
+  pr = pv - 5;
+  pr.Memory().TransferToCPU();
+
+  cmm::Stream::This().Synchronize();
+  for (std::size_t n = 0; n < pv.Size(); ++n) {
+    ASSERT_EQ(pr.At(n), 6);
+  }
+
+  pv = std::move(pr);
+
+  // Mul test
+  pv.Memory().TransferToGPU();
+  pr = pv * 4;
+  pr.Memory().TransferToCPU();
+
+  cmm::Stream::This().Synchronize();
+  for (std::size_t n = 0; n < pv.Size(); ++n) {
+    ASSERT_EQ(pr.At(n), 24);
+  }
+
+  pv = std::move(pr);
+
+  // Div test
+  pv.Memory().TransferToGPU();
+  pr = pv / 2;
+  pr.Memory().TransferToCPU();
+
+  cmm::Stream::This().Synchronize();
+  for (std::size_t n = 0; n < pv.Size(); ++n) {
+    ASSERT_EQ(pr.At(n), 12);
   }
 }
