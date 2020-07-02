79d354e7a21e27a8a1624bc599aedc3c90025154
Zeek Oni <web@cjhanks.name>
2020-07-02 09:48:50 -0700

    More pretty examples in unit test.

--- test/cmm/matrix.cu
+++ test/cmm/matrix.cu
@@ -269,3 +269,52 @@ TEST(MatrixCU, PointwiseInPlace) {
     ASSERT_EQ(a0.At(n), 2);
   }
 }
+
+TEST(MatrixCU, Mat2D_FullTest) {
+  // - Initialize
+  cmm::PinMatrix<double, 2> a0(1024, 512);
+  cmm::PinMatrix<double, 2> a1(1024, 512);
+
+  for (std::size_t i = 0; i < a0.Size(0); ++i) {
+    for (std::size_t j = 0; j < a0.Size(1); ++j) {
+      a0.At(i, j) = i + j;
+      a1.At(i, j) = i - j;
+    }
+  }
+
+  a0.Memory().TransferToGPU();
+  a1.Memory().TransferToGPU();
+
+  // - Let's perform various complicated operations.
+  auto a2 = a0 + 32.0;
+
+  a1 /= a2;
+  a2  = a0 * a1;
+  a0 += a1 - a2;
+
+  auto a3 = (a0 += 4.0) - a1;
+
+  a0.Memory().TransferToCPU();
+  a1.Memory().TransferToCPU();
+  a2.Memory().TransferToCPU();
+  a3.Memory().TransferToCPU();
+
+  for (std::size_t i = 0; i < a0.Size(0); ++i) {
+    for (std::size_t j = 0; j < a0.Size(1); ++j) {
+      double a0_ = i + j;
+      double a1_ = i - j;
+      double a2_ = a0_ + 32.0;
+
+      a1_ /= a2_;
+      a2_  = a0_ * a1_;
+      a0_ += a1_ - a2_;
+
+      double a3_ = (a0_ += 4.0) - a1_;
+
+      ASSERT_NEAR(a0_, a0.At(i, j), 1e-9);
+      ASSERT_NEAR(a1_, a1.At(i, j), 1e-9);
+      ASSERT_NEAR(a2_, a2.At(i, j), 1e-9);
+      ASSERT_NEAR(a3_, a3.At(i, j), 1e-9);
+    }
+  }
+}
